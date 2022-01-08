efb048ce48cd1e09024305142f28c5ee0ff4218d
unokun <unokun@gmail.com>
2022-01-09 06:21:50 +0900

    refactoring to use tcp listener

--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,10 @@
 // }
 
 use zero2prod::run;
+use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind address");
+    run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -9,11 +9,13 @@
 //
 // You can inspect what code gets generated using 
 // `cargo expand --test health_check` (<- name of the test file)
+use std::net::TcpListener;
+
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
     // spawn_app().await.expect("Failed to spawn our app.");
-    spawn_app();
+    let address = spawn_app();
 
     // We need to bring in `reqwest` 
     // to perform HTTP requests against our application.
@@ -21,7 +23,8 @@ async fn health_check_works() {
 
     // Act
     let response = client
-            .get("http://127.0.0.1:8000/health_check")
+            // .get("http://127.0.0.1:8000/health_check")
+            .get(&format!("{}/health_check", &address))
             .send()
             .await
             .expect("Failed to execute request.");
@@ -33,8 +36,11 @@ async fn health_check_works() {
 
 // Launch our application in the background ~somehow~
 // async fn spawn_app() -> std::io::Result<()> {
-fn spawn_app() {
-        // zero2prod::run().await
-    let server = zero2prod::run().expect("Failed to bind address");
+fn spawn_app() -> String {
+    // zero2prod::run().await
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind address");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
 }
