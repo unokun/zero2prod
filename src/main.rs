5413c6b39e10c03c25a8ed336daba37028b57920
unokun <unokun@gmail.com>
2022-01-08 11:35:47 +0900

    add test for health check API

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+// use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
+// async fn health_check() -> impl Responder {
+//     HttpResponse::Ok()
+// }
+
+// #[tokio::main]
+// async fn main() -> std::io::Result<()> {
+//     HttpServer::new(|| {
+//         App::new()
+//             .route("/health_check", web::get().to(health_check))
+//     })
+//     .bind("127.0.0.1:8000")?
+//     .run()
+//     .await
+// }
+
+use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    run()?.await
 }
